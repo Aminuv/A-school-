3bf7b51a0b2346d6c9d1bdc5659e80c90373d6e6
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-12-03 23:36:23 +0000

    first commit

---
+++ Fron-End/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      
+    <div> 
+      <img src="https://cdn.educba.com/academy/wp-content/uploads/2020/01/React-Native-Architecture-1.jpg.webp" alt="" />
+    <h1>el</h1>
+    </div> 
+    <div>
+    <button className="text"> 
+    </button>
+    </div>
+    
+    
+    
+    </div>
+
+  )
+}
+
