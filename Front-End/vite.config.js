c1f7945b0bb353b98c193b3ff31fc0e8696e0fbc
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-12-06 08:52:44 +0000

    complite sign up and conecter to db

--- Front-End/vite.config.js
+++ Front-End/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({ // vite.config.js
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
