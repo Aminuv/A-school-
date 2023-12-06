1254176223228c2fc0c3359cdf0b676d0254bb14
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-12-06 12:21:14 +0000

    create sign in API route

--- Back-End/controllers/auth.controller.js
+++ Back-End/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 // after getting the user from the database, we compare the password from the request with the password from the database
 // using bcrypt.compare() method. If the passwords match, we return a response with the user data and a message
@@ -12,6 +13,24 @@ export const signup = async (req, res, next) => {
         await newUser.save()
         res.status(201).json("User created successfully!");
     } catch (error) {
-        next(errorHandler(550, "error from Function"));
+        next(error);
     }
 };
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'User not found!'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: pass, ...rest } = validUser._doc;
+        res
+        .cookie('access_token', token, { httpOnly: true })
+        .status(200)
+        .json(rest);
+    } catch (error) {
+        next(error);
+    }
+}
