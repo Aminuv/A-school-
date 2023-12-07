b46d976b1c6b1ad56939f5bd4fd9c6642b447fd0
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-12-07 19:05:02 +0000

    creat courses

--- Back-End/index.js
+++ Back-End/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.listen(3000, () => console.log('Server is running in port 30000'));
 
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
+app.use('/api/listings', listingRouter);   
 
 // the moddelwre for handling errors.
 app.use((err, req, res, next) => {
