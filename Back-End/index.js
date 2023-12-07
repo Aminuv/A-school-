67c7ed883d6bb6eee939b411b20fce0d62f4a9f0
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-12-07 17:17:29 +0000

    create update user api route 01

--- Back-End/index.js
+++ Back-End/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URI).then(() => console.log('DB is connected')).catch((err) => console.log(err));
@@ -12,7 +13,9 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => console.log('Server is running'));
+app.use(cookieParser());
+
+app.listen(3000, () => console.log('Server is running in port 30000'));
 
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
